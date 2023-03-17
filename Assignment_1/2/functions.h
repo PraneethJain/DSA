c10fd7212359f7558697598e3345793eb31b006d
PraneethJain <praneethjain005@gmail.com>
2023-03-17 16:30:50 +0530

    Fixed intersectionArray

--- Assignment_1/2/functions.h
+++ Assignment_1/2/functions.h
@@ -6,7 +6,7 @@ extern int intersection_array_size;
 
 int str_length(const char *string);
 
-int *IntersectionArray(int *Arr1, int *Arr2, int lenArr1, int lenArr2);
+int *intersectionArray(int *Arr1, int *Arr2, int lenArr1, int lenArr2);
 int countCharOccurences(const char *str, int length, char ch);
 char findFirstNonRepeatingChar(const char *str, int length);
 char *findLongestCommonPrefix(char **strs, int numStr, int maxLen);
--- Assignment_1/2/main.c
+++ Assignment_1/2/main.c
@@ -2,33 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
   int T;
   scanf("%i", &T);
   char choice[100];
-  for (int i = 0; i < T; ++i) {
+  for (int i = 0; i < T; ++i)
+  {
     scanf("%s", choice);
     char x = choice[4];
-    if (x == '1') {
+    if (x == '1')
+    {
 
       int n1, n2;
       scanf("%i %i", &n1, &n2);
       int arr1[n1];
       int arr2[n2];
-      for (int i = 0; i < n1; ++i) {
+      for (int i = 0; i < n1; ++i)
+      {
         scanf("%i", &arr1[i]);
       }
-      for (int i = 0; i < n2; ++i) {
+      for (int i = 0; i < n2; ++i)
+      {
         scanf("%i", &arr2[i]);
       }
-      int *res = IntersectionArray(arr1, arr2, n1, n2);
+      int *res = intersectionArray(arr1, arr2, n1, n2);
 
-      for (int i = 0; i < intersection_array_size; ++i) {
+      for (int i = 0; i < intersection_array_size; ++i)
+      {
         printf("%i ", res[i]);
       }
       printf("\n");
-
-    } else if (x == '2') {
+    }
+    else if (x == '2')
+    {
 
       char str[10000];
       scanf("%s", str);
@@ -36,42 +43,49 @@ int main() {
       scanf("%c", &c);
       scanf("%c", &c);
       printf("%i\n", countCharOccurences(str, str_length(str), c));
-
-    } else if (x == '3') {
+    }
+    else if (x == '3')
+    {
 
       char str[10000];
       scanf("%s", str);
       printf("%c\n", findFirstNonRepeatingChar(str, str_length(str)));
-
-    } else if (x == '4') {
+    }
+    else if (x == '4')
+    {
 
       int n;
       scanf("%i", &n);
       char **strings = (char **)malloc(sizeof(char *) * n);
-      for (int i = 0; i < n; ++i) {
+      for (int i = 0; i < n; ++i)
+      {
         *(strings + i) =
             (char *)malloc(sizeof(char) * 100); // Max size of string is 100
       }
 
-      for (int i = 0; i < n; ++i) {
+      for (int i = 0; i < n; ++i)
+      {
         scanf("%s", strings[i]);
       }
 
       char *prefix = findLongestCommonPrefix(strings, n, 100);
 
       printf("%s\n", prefix);
-
-    } else if (x == '5') {
+    }
+    else if (x == '5')
+    {
 
       int n;
       scanf("%i", &n);
       int arr[n];
-      for (int i = 0; i < n; ++i) {
+      for (int i = 0; i < n; ++i)
+      {
         scanf("%i", &arr[i]);
       }
 
       int *indices = MaxMin(arr, n);
-      for (int i = 0; i < max_min_array_size; ++i) {
+      for (int i = 0; i < max_min_array_size; ++i)
+      {
         printf("%i ", indices[i]);
       }
       printf("\n");
