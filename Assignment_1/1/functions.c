1980e8d12e5c2e1bf439ad15e2299daa8f25c5cd
PraneethJain <praneethjain005@gmail.com>
2023-03-18 09:53:23 +0530

    formatted

--- Assignment_1/1/functions.c
+++ Assignment_1/1/functions.c
@@ -4,22 +4,27 @@
 
 int unique_elements_count = 0;
 
-void swap(char *x, char *y) {
+void swap(char *x, char *y)
+{
   char temp = *x;
   *x = *y;
   *y = temp;
 }
 
-void reverseString(char *str, int length) {
-  for (int i = 0; i < length / 2; ++i) {
+void reverseString(char *str, int length)
+{
+  for (int i = 0; i < length / 2; ++i)
+  {
     swap(&str[i], &str[length - i - 1]);
   }
 }
 
-int *uniqueElements(int *arr, int length) {
+int *uniqueElements(int *arr, int length)
+{
   int map[1000001] = {0};
   int *res = (int *)malloc(sizeof(int) * length);
-  for (int i = 0; i < length; ++i) {
+  for (int i = 0; i < length; ++i)
+  {
     map[arr[i]]++;
     if (map[arr[i]] == 1)
       res[unique_elements_count++] = arr[i];
@@ -28,26 +33,31 @@ int *uniqueElements(int *arr, int length) {
   return res;
 }
 
-int ceil_log_10(int x) {
+int ceil_log_10(int x)
+{
   int res = 1;
-  while (x) {
+  while (x)
+  {
     x /= 10;
     res += 1;
   }
   return res;
 }
 
-char *int_to_str(int x) {
+char *int_to_str(int x)
+{
   char *s = (char *)malloc((ceil_log_10(x)) * sizeof(char));
   sprintf(s, "%i", x);
 
   return s;
 }
 
-int str_length(const char *string) {
+int str_length(const char *string)
+{
   if (string[0] == '\0')
     return 0;
-  else {
+  else
+  {
     int res = 0;
     while (string[++res] != '\0')
       ;
@@ -55,31 +65,39 @@ int str_length(const char *string) {
   }
 }
 
-void concat(char *destination, const char *source) {
+void concat(char *destination, const char *source)
+{
   int len_destination = str_length(destination);
   int len_source = str_length(source);
 
-  for (int i = len_destination; i <= len_destination + len_source; ++i) {
+  for (int i = len_destination; i <= len_destination + len_source; ++i)
+  {
     destination[i] = source[i - len_destination];
   }
 }
 
-char *compressString(char *str, int length) {
+char *compressString(char *str, int length)
+{
 
   char prev[2] = {str[0], '\0'};
   int cur_count = 1;
   char cur;
   char *res = (char *)malloc(sizeof(char) * (2 * length + 1));
-  for (int i = 0; i < length; ++i) {
+  for (int i = 0; i < length; ++i)
+  {
     cur = str[i];
-    if (i == 0) {
+    if (i == 0)
+    {
       continue;
     }
-    if (cur != prev[0]) {
+    if (cur != prev[0])
+    {
       concat(res, prev);
       concat(res, int_to_str(cur_count));
       cur_count = 1;
-    } else {
+    }
+    else
+    {
       cur_count++;
     }
     prev[0] = cur;
@@ -91,13 +109,17 @@ char *compressString(char *str, int length) {
   return res;
 }
 
-int **transpose(int **matrix, int NumRow, int NumCol) {
+int **transpose(int **matrix, int NumRow, int NumCol)
+{
   int **res = (int **)malloc(sizeof(int *) * NumCol);
-  for (int i = 0; i < NumCol; ++i) {
+  for (int i = 0; i < NumCol; ++i)
+  {
     *(res + i) = (int *)malloc(sizeof(int) * NumRow);
   }
-  for (int i = 0; i < NumCol; ++i) {
-    for (int j = 0; j < NumRow; ++j) {
+  for (int i = 0; i < NumCol; ++i)
+  {
+    for (int j = 0; j < NumRow; ++j)
+    {
       res[i][j] = matrix[j][i];
     }
   }
@@ -105,9 +127,12 @@ int **transpose(int **matrix, int NumRow, int NumCol) {
   return res;
 }
 
-void print_matrix(int **matrix, int NumRow, int NumCol) {
-  for (int i = 0; i < NumRow; ++i) {
-    for (int j = 0; j < NumCol; ++j) {
+void print_matrix(int **matrix, int NumRow, int NumCol)
+{
+  for (int i = 0; i < NumRow; ++i)
+  {
+    for (int j = 0; j < NumCol; ++j)
+    {
       printf("%i ", matrix[i][j]);
     }
     printf("\n");
--- Assignment_1/1/main.c
+++ Assignment_1/1/main.c
@@ -2,15 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
   int T;
   scanf("%i", &T);
 
   char choice[10];
-  for (int i = 0; i < T; ++i) {
+  for (int i = 0; i < T; ++i)
+  {
     scanf("%s", choice);
     char x = choice[4];
-    if (x == '1') {
+    if (x == '1')
+    {
 
       int n;
       scanf("%i", &n);
@@ -18,8 +21,9 @@ int main() {
       scanf("%s", s);
       reverseString(s, n);
       printf("%s\n", s);
-
-    } else if (x == '2') {
+    }
+    else if (x == '2')
+    {
 
       int n;
       scanf("%i", &n);
@@ -27,39 +31,48 @@ int main() {
       scanf("%s", s);
       char *compressedString = compressString(s, n);
       printf("%s\n", compressedString);
-
-    } else if (x == '3') {
+    }
+    else if (x == '3')
+    {
 
       int n;
       scanf("%i", &n);
       int arr[n];
-      for (int i = 0; i < n; ++i) {
+      for (int i = 0; i < n; ++i)
+      {
         scanf("%i", &arr[i]);
       }
       int *unique_arr = uniqueElements(arr, n);
-      for (int i = 0; i < unique_elements_count; ++i) {
+      for (int i = 0; i < unique_elements_count; ++i)
+      {
         printf("%i ", unique_arr[i]);
       }
       printf("\n");
-
-    } else if (x == '4') {
+    }
+    else if (x == '4')
+    {
 
       int R, C;
       scanf("%i %i", &R, &C);
       int **matrix = (int **)malloc(sizeof(int *) * R);
-      for (int i = 0; i < R; ++i) {
+      for (int i = 0; i < R; ++i)
+      {
         *(matrix + i) = (int *)malloc(sizeof(int) * C);
       }
 
-      for (int i = 0; i < R; ++i) {
-        for (int j = 0; j < C; ++j) {
+      for (int i = 0; i < R; ++i)
+      {
+        for (int j = 0; j < C; ++j)
+        {
           scanf("%i", &matrix[i][j]);
         }
       }
 
       int **transposed_matrix = transpose(matrix, R, C);
-      for (int i = 0; i < C; ++i) {
-        for (int j = 0; j < R; ++j) {
+      for (int i = 0; i < C; ++i)
+      {
+        for (int j = 0; j < R; ++j)
+        {
           printf("%i ", transposed_matrix[i][j]);
         }
         printf("\n");
