371c1f598b13c14246a22eeda0d4da29723e4de3
PraneethJain <praneethjain005@gmail.com>
2023-04-26 18:32:31 +0530

    removed testing code

--- Assignment_3/1/main.c
+++ Assignment_3/1/main.c
@@ -7,17 +7,7 @@ int main()
   unsigned char buffer[buffer_length];
   Arena a = {0};
   arena_init(&a, buffer, buffer_length);
-  int *x = a.arena_alloc(&a, sizeof(int) * 16);
-  for (int i = 0; i < 16; ++i)
-  {
-    x[i] = i;
-  }
 
-  for (int i = 0; i < 16; ++i)
-  {
-    printf("%i ", x[i]);
-  }
-  printf("\n");
-
+  a.arena_free(&a);
   return 0;
 }
