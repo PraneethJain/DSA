51019abd7b59657170737f84caccfd8bf4674f8c
PraneethJain <praneethjain005@gmail.com>
2023-04-26 18:20:47 +0530

    moved arena to hash.c

--- Assignment_3/1/main.c
+++ Assignment_3/1/main.c
@@ -1,28 +1,6 @@
 #include "hash.h"
 #include <stdio.h>
 
-typedef struct Arena
+int main()
 {
-  unsigned char *buffer;
-  size_t buffer_length;
-  size_t offset;
-} Arena;
-
-void arena_init(Arena *a, unsigned char *buffer, size_t buffer_length)
-{
-  a->buffer = buffer;
-  a->buffer_length = buffer_length;
-  a->offset = 0;
-}
-
-void *arena_alloc(Arena *a, size_t size)
-{
-  void *allocated = (void *)(a->buffer + a->offset);
-  a->offset += size;
-  return allocated;
-}
-
-void arena_free(Arena *a)
-{
-  a->offset = 0;
 }
