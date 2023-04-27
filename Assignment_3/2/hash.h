9f542918f4427b39efdec96c387ee6fde38290c6
PraneethJain <praneethjain005@gmail.com>
2023-04-27 13:45:53 +0530

    Arena boilerplate

---
+++ Assignment_3/2/hash.h
@@ -0,0 +1,19 @@
+#ifndef __HASH_H
+#define __HASH_H
+
+#include <stdio.h>
+
+typedef struct Arena
+{
+  unsigned char *buffer;
+  size_t buffer_length;
+  size_t offset;
+  void *(*arena_alloc)(struct Arena *a, size_t size);
+  void (*arena_free)(struct Arena *a);
+} Arena;
+
+void arena_init(Arena *a, unsigned char *buffer, size_t buffer_length);
+void *arena_alloc(Arena *a, size_t size);
+void arena_free(Arena *a);
+
+#endif
