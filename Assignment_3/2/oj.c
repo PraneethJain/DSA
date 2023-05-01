108f8e693397cdce194d36e812d5b0e339470a42
PraneethJain <praneethjain005@gmail.com>
2023-05-01 19:01:08 +0530

    actually working (not hardcoded)

--- Assignment_3/2/oj.c
+++ Assignment_3/2/oj.c
@@ -2,8 +2,11 @@
 #define __HASH_H
 
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define int long long
 
 int *compute_powers(int num, size_t n);
 int **hash(char *str, size_t length, int *powers);
@@ -11,14 +14,20 @@ bool is_palindrome(int *forward_hash, int *reverse_hash, int x, int y, int lengt
 
 #endif
 
-const int size = 1572869;
+const int size = 3145739;
+// const int size = 12289;
+// const int size = 12582917;
 
 int *compute_powers(int num, size_t n)
 {
   int *powers = (int *)malloc(sizeof(int) * n);
   powers[0] = 1;
   for (size_t i = 1; i < n; ++i)
+  {
     powers[i] = (powers[i - 1] * num) % size;
+    if (powers[i] < 0)
+      powers[i] += size;
+  }
 
   return powers;
 }
@@ -36,33 +45,38 @@ int **hash(char *str, size_t length, int *powers)
   for (int i = 1; i < length; ++i)
   {
     res[0][i] = (res[0][i - 1] + ((str[i] - 'a' + 1) * powers[i]) % size) % size;
+    if (res[0][i] < 0)
+      res[0][i] += size;
     res[1][i] = (res[1][i - 1] + ((str[length - 1 - i] - 'a' + 1) * powers[i]) % size) % size;
+    if (res[1][i] < 0)
+      res[1][i] += size;
   }
-
-  return res;
-}
-
-// Temporary
-int raise(int base, int exp)
-{
-  int res = 1;
-  for (int i = 0; i < exp; ++i)
-    res = (res * base) % size;
 
   return res;
 }
 
 bool is_palindrome(int *forward_hash, int *reverse_hash, int x, int y, int length, int *powers)
 {
-  int t1 = forward_hash[y] - (x == 0 ? 0 : forward_hash[x - 1]);
-  int t2 = reverse_hash[length - 1 - x] - (y == length - 1 ? 0 : reverse_hash[length - 2 - y]);
+  int t1 = (forward_hash[y] - ((x == 0) ? (0) : (forward_hash[x - 1]))) % size;
+  int t2 = (reverse_hash[length - 1 - x] - ((y == length - 1) ? (0) : (reverse_hash[length - 2 - y]))) % size;
 
-  // printf("%i %i\n", t1, t2);
-  // printf("%i\n", x + y + 1 - length);
+  if (t1 < 0)
+    t1 += size;
+  if (t2 < 0)
+    t2 += size;
+
   if (x + y + 1 - length > 0)
+  {
     t2 = (t2 * powers[x + y + 1 - length]) % size;
+    if (t2 < 0)
+      t2 += size;
+  }
   else
+  {
     t1 = (t1 * powers[length - x - y - 1]) % size;
+    if (t1 < 0)
+      t1 += size;
+  }
 
   return t1 == t2;
 }
@@ -76,44 +90,44 @@ bool is_palindrome_slow(char *str, size_t length)
   return true;
 }
 
-int main()
+int32_t main()
 {
   size_t length;
   int q;
-  scanf("%zu %i", &length, &q);
+  scanf("%zu %lli", &length, &q);
   char str[length + 1];
   scanf("%s", str);
 
   size_t l, r;
-  if (length < 100000)
-  {
-    for (size_t i = 0; i < q; ++i)
-    {
-      scanf("%zu %zu", &l, &r);
-      if (is_palindrome_slow(str + l - 1, r - l + 1))
-        printf("YES");
-      else
-        printf("NO");
-      printf("\n");
-    }
-  }
-  else
-  {
-    int *powers = compute_powers(2, length);
-    int **res = hash(str, length, powers);
-    int *forward_hash = res[0];
-    int *reverse_hash = res[1];
+  // if (length < 100000)
+  // {
+  //   for (size_t i = 0; i < q; ++i)
+  //   {
+  //     scanf("%zu %zu", &l, &r);
+  //     if (is_palindrome_slow(str + l - 1, r - l + 1))
+  //       printf("YES");
+  //     else
+  //       printf("NO");
+  //     printf("\n");
+  //   }
+  // }
+  // else
+  // {
+  int *powers = compute_powers(37, length);
+  int **res = hash(str, length, powers);
+  int *forward_hash = res[0];
+  int *reverse_hash = res[1];
 
-    for (size_t i = 0; i < q; ++i)
-    {
-      scanf("%zu %zu", &l, &r);
-      if (is_palindrome(forward_hash, reverse_hash, l - 1, r - 1, length, powers))
-        printf("YES");
-      else
-        printf("NO");
-      printf("\n");
-    }
+  for (size_t i = 0; i < q; ++i)
+  {
+    scanf("%zu %zu", &l, &r);
+    if (is_palindrome(forward_hash, reverse_hash, l - 1, r - 1, length, powers))
+      printf("YES");
+    else
+      printf("NO");
+    printf("\n");
   }
+  // }
 
   return 0;
 }
