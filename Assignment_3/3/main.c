245ddec84c127df87561901d0423db75bf84adf7
PraneethJain <praneethjain005@gmail.com>
2023-05-08 14:18:23 +0530

    main arena init

---
+++ Assignment_3/3/main.c
@@ -0,0 +1,14 @@
+#include "heap.h"
+#include <stdio.h>
+
+int main()
+{
+  const size_t buffer_length = 1024 * 1024;
+  unsigned char buffer[buffer_length];
+  Arena a = {0};
+  arena_init(&a, buffer, buffer_length);
+
+  a.arena_free(&a);
+
+  return 0;
+}
