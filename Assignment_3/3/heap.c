45128523a65da6df7918be5a9116afadd5211f76
PraneethJain <praneethjain005@gmail.com>
2023-05-08 14:17:44 +0530

    added arena

--- Assignment_3/3/heap.c
+++ Assignment_3/3/heap.c
@@ -1,1 +1,22 @@
 #include "heap.h"
+
+void arena_init(Arena *a, unsigned char *buffer, size_t buffer_length)
+{
+  a->buffer = buffer;
+  a->buffer_length = buffer_length;
+  a->offset = 0;
+  a->arena_alloc = arena_alloc;
+  a->arena_free = arena_free;
+}
+
+void *arena_alloc(Arena *a, size_t size)
+{
+  void *allocated = (void *)(a->buffer + a->offset);
+  a->offset += size;
+  return allocated;
+}
+
+void arena_free(Arena *a)
+{
+  a->offset = 0;
+}
--- Assignment_3/3/heap.h
+++ Assignment_3/3/heap.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 
+typedef struct Arena
+{
+  unsigned char *buffer;
+  size_t buffer_length;
+  size_t offset;
+  void *(*arena_alloc)(struct Arena *a, size_t size);
+  void (*arena_free)(struct Arena *a);
+} Arena;
+
+void arena_init(Arena *a, unsigned char *buffer, size_t buffer_length);
+void *arena_alloc(Arena *a, size_t size);
+void arena_free(Arena *a);
+
 typedef struct heap
 {
   size_t capacity;
