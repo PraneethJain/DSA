120b39b74b13e60a3aa01017f2539ba3511e8fee
PraneethJain <praneethjain005@gmail.com>
2023-05-21 21:25:22 +0530

    input better

--- STL/test.cpp
+++ STL/test.cpp
@@ -1,9 +1,13 @@
+#include <ios>
 #include <iostream>
 
 using ll = long long;
 
 int main()
 {
+  std::ios_base::sync_with_stdio(false); // Removes c and c++ input compatability
+  std::cin.tie(NULL);                    // Doesn't cout until flushed
+
   std::cout << "Test" << std::endl;
 
   ll x{};
